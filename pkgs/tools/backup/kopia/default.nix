5331a3db9dd8511a91f248c9bc0011e51efb0f43
David Knaack <davidkna@users.noreply.github.com>
2021-04-18 10:56:56 +0200

    kopia: 0.7.3 -> 0.8.2

--- pkgs/tools/backup/kopia/default.nix
+++ pkgs/tools/backup/kopia/default.nix
@@ -1,17 +1,17 @@
-{ lib, buildGoModule, fetchFromGitHub, coreutils }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
   pname = "kopia";
-  version = "0.7.3";
+  version = "0.8.2";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "1dnk764y71c9k9nghn9q06f2zz9igsvm4z826azil2d58h5d06j6";
+    sha256 = "1fa8pacyydzgqnl4ghcj1fvnxg3v7ab24wy8wyd779rj1xxqmdrf";
   };
 
-  vendorSha256 = "1mnhq6kn0pn67l55a9k6irmjlprr295218nms3klsk2720syzdwq";
+  vendorSha256 = "04lqnk1y8464r7d7w2qx7lbg3azyyzr21z583arjrnp6ichbjlnl";
 
   doCheck = false;
 
@@ -21,12 +21,6 @@ buildGoModule rec {
     -ldflags=
        -X github.com/kopia/kopia/repo.BuildVersion=${version}
        -X github.com/kopia/kopia/repo.BuildInfo=${src.rev}
-  '';
-
-  postConfigure = ''
-    # speakeasy hardcodes /bin/stty https://github.com/bgentry/speakeasy/issues/22
-    substituteInPlace vendor/github.com/bgentry/speakeasy/speakeasy_unix.go \
-      --replace "/bin/stty" "${coreutils}/bin/stty"
   '';
 
   meta = with lib; {
